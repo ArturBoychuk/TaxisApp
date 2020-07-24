ec1f810ca82eb7c2e018750443c064c003681f68
artur.boychuk93@gmail.com <artur.boychuk93@gmail.com>
2020-07-25 01:04:22 +0300

    Initial commit

---
+++ app/src/main/java/com/artur/taxisapp/ChooseModeActivity.java
@@ -0,0 +1,31 @@
+package com.artur.taxisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ChooseModeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mode);
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(
+                ChooseModeActivity.this,
+                DriverSignInActivity.class
+        ));
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChooseModeActivity.this,
+                PassengerSignInActivity.class));
+    }
+}
